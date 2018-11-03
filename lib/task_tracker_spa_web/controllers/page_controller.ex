3827ffd7410104d52de9ba1e3e1f821197eb8160
jzybert <jmzybert@gmail.com>
2018-11-03 11:57:56 -0400

    Add "React component loading..." screen

--- lib/task_tracker_spa_web/controllers/page_controller.ex
+++ lib/task_tracker_spa_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule TaskTrackerSpaWeb.PageController do
   use TaskTrackerSpaWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    tasks = TaskTrackerSpa.Tasks.list_tasks()
+    |> Enum.map(&(Map.take(&1, [:id, :title, :desc, :time_worked, :is_complete])))
+    render conn, "index.html", tasks: tasks
   end
 end
