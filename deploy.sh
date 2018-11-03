1333fa4c86ffa11eba396be91e5b9da8aa34334e
jzybert <jmzybert@gmail.com>
2018-11-03 11:34:05 -0400

    Add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4797
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mkdir -p ~/.config
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+echo "Starting app..."
+
+_build/prod/rel/task_tracker/bin/task_tracker foreground
