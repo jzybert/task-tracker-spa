1aef91117ccfef4995bfce6673800a5d214416f8
jzybert <jmzybert@gmail.com>
2018-11-06 19:24:34 -0500

    Turn LoginButton into Redux

--- assets/js/components/LoginButton.jsx
+++ assets/js/components/LoginButton.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default function LoginButton(props) {
-  let {root, sessionCreated, session} = props;
+import api from '../api';
 
-  function login() {
-    let email = $('#login-email').val();
-    let pw = $('#login-password').val();
-    root.create_session(email, pw);
-  }
+class LoginButton extends Component {
+  render() {
+    let {sessionCreated} = this.props;
 
-  if (!sessionCreated) {
-    return (
-      <div className="form-inline my-2">
-        <input id="login-email" type="email" placeholder="Enter email..." />
-        <input id="login-password" type="password" placeholder="Enter password..." />
-        <button className="btn btn-secondary" onClick={login}>Login</button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="form-inline my-2">
-        <p>Welcome, User</p>
-        <button className="btn btn-secondary">Logout</button>
-      </div>
-    );
+    function login() {
+      let email = $('#login-email').val();
+      let pw = $('#login-password').val();
+      api.create_session(email, pw);
+    }
+
+    if (!sessionCreated) {
+      return (
+        <div className="form-inline my-2">
+          <input id="login-email" type="email" placeholder="Enter email..."/>
+          <input id="login-password" type="password"
+                 placeholder="Enter password..."/>
+          <button className="btn btn-secondary" onClick={login}>Login</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="form-inline my-2">
+          <p>Welcome, User</p>
+          <button className="btn btn-secondary">Logout</button>
+        </div>
+      );
+    }
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    sessionCreated: state.sessionCreated
+  }
+};
+
+export default connect(mapStateToProps)(LoginButton);
