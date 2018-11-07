53f7eed5b3646b2bdb6348beacdd017fd56ed75e
jzybert <jmzybert@gmail.com>
2018-11-06 19:32:47 -0500

    Add error handling

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,51 +4,70 @@ import store from './reducers/store';
 import {CREATE_NEW_SESSION, UPDATE_TASKS, UPDATE_USERS} from "./consts/types";
 
 class TaskTrackerServer {
-  create_session(email, password) {
-    $.ajax("/api/v1/sessions", {
-      method: "post",
-      dataType: "json",
-      contentType: "application/json; charset=UTF-8",
-      data: JSON.stringify({email, password}),
-      success: response => {
-        store.dispatch({
-          type: CREATE_NEW_SESSION,
-          session: response.data
-        });
-      }
-    });
-  }
-
-  fetch_path(path, success_callback) {
+  get(path, success_callback, error_callback) {
     $.ajax(path, {
       method: "get",
       dataType: "json",
       contentType: "application/json; charset=UTF-8",
       data: "",
-      success: success_callback
-    });
+      success: success_callback,
+      error: error_callback
+    })
+  }
+
+  post(path, data, success_callback, error_callback) {
+    $.ajax(path, {
+      method: "post",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      data: data,
+      success: success_callback,
+      error: error_callback
+    })
+  }
+
+  create_session(email, password) {
+    this.post(
+      "/api/v1/sessions",
+      JSON.stringify({email, password}),
+      response => {
+        store.dispatch({
+          type: CREATE_NEW_SESSION,
+          session: response.data
+        });
+      },
+      (xhr, status, error) => {
+        console.log("Error: could not create session");
+      }
+    )
   }
 
   fetch_tasks() {
-    this.fetch_path(
+    this.get(
       "/api/v1/tasks",
       response => {
         store.dispatch({
           type: UPDATE_TASKS,
           tasks: response.data
         });
+      },
+      (xhr, status, error) => {
+        console.log("Error: could not fetch tasks");
       }
     )
   }
 
   fetch_users() {
-    this.fetch_path(
+    this.get(
       "/api/v1/users",
       response => {
         store.dispatch({
           type: UPDATE_USERS,
           users: response.data
         });
+      },
+      (xhr, status, error) => {
+        console.log("Error: could not fetch users");
       }
     )
   }
